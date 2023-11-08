7c57e14e09a85155cc6a436baf97d96149e5e89c
sikatram <martakisalex@gmail.com>
2023-11-07 19:56:21 -0800

    feat(template): add queue_template.py

---
+++ data-structure/queue/template/queue_template.py
@@ -0,0 +1,24 @@
+class Queue:
+    def __init__(self):
+        # Initialize an empty list to hold queue items
+        pass
+
+    def enqueue(self, item):
+        # Add an item to the back of the queue
+        pass
+
+    def dequeue(self):
+        # Remove and return the front item from the queue, if not empty
+        pass
+
+    def front(self):
+        # Return front item without removal, if not empty
+        pass
+
+    def is_empty(self):
+        # Check if the queue is empty
+        pass
+
+    def size(self):
+        # Return the number of items in the queue
+        pass

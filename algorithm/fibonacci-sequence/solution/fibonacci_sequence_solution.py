0fed7bd79c3b9b1f8e1630247de4ffc2a5d6b0a3
sikatram <martakisalex@gmail.com>
2023-11-07 20:02:13 -0800

    feat(solution): add fibonacci_sequence_solution.py

---
+++ algorithm/fibonacci-sequence/solution/fibonacci_sequence_solution.py
@@ -0,0 +1,9 @@
+def fibonacci_sequence(n):
+    # Display the fibonacci sequence up to n elements
+    sequence = [0, 1]
+
+    while len(sequence) < n:
+        next_number = sequence[-1] + sequence[-2]
+        sequence.append(next_number)
+
+    return sequence

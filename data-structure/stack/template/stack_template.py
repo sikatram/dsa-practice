1db94603e07ea987b8d4bed513341d739e42fe90
sikatram <martakisalex@gmail.com>
2023-11-07 19:53:21 -0800

    feat(template): add stack_template.py

---
+++ data-structure/stack/template/stack_template.py
@@ -0,0 +1,24 @@
+class Stack:
+    def __init__(self):
+        # Initialize an empty list to hold stack items
+        pass
+
+    def push(self, item):
+        # Add an item to the top of the stack
+        pass
+
+    def pop(self):
+        # Remove and return the top item from the stack, if not empty
+        pass
+
+    def peek(self):
+        # Return the top item from the stack without removing it, if not empty
+        pass
+
+    def is_empty(self):
+        # Check if the stack is empty
+        pass
+
+    def size(self):
+        # Return the number of items in the stack
+        pass

ee9a9dd3eef952e9051e83c6252702b947e5af6d
sikatram <martakisalex@gmail.com>
2023-11-07 19:52:42 -0800

    feat(template): add linked_list_template.py

---
+++ data-structure/linked-list/template/linked_list_template.py
@@ -0,0 +1,30 @@
+class Node:
+    def __init__(self, data):
+        # Initialize a node with data and a reference to the next node
+        pass
+
+
+class LinkedList:
+    def __init__(self):
+        # Initialize an empty linked list with a head node
+        pass
+
+    def display(self):
+        # Display linked list elements
+        pass
+
+    def append(self, data):
+        # Append new node with data to end of linked list
+        pass
+
+    def prepend(self, data):
+        # Prepend new node with data to beginning of linked list
+        pass
+
+    def remove(self, key):
+        # Remove a node with the given key value from the linked list
+        pass
+
+    def reverse(self):
+        # Reverse linked list in-place
+        pass

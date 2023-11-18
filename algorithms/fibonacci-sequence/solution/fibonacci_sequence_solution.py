e8b039869376bae48234b258cf3fe8fc7e553855
sikatram <martakisalex@gmail.com>
2023-11-18 15:00:20 -0800

    fix: edge cases in fibonacci_sequence_solution.py

--- algorithms/fibonacci-sequence/solution/fibonacci_sequence_solution.py
+++ algorithms/fibonacci-sequence/solution/fibonacci_sequence_solution.py
@@ -1,5 +1,10 @@
 def fibonacci_sequence(n):
     # Display the fibonacci sequence up to n elements
+    if n <= 0:
+        return []
+    elif n == 1:
+        return [0]
+
     sequence = [0, 1]
 
     while len(sequence) < n:
